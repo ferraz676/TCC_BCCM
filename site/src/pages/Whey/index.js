16de98434113adcfba4591eb5c0f99b660fe4cf7
ferraz676 <mat_ferraz12@hotmail.com>
2023-10-19 14:37:50 -0300

    ok whey

--- site/src/pages/Whey/index.js
+++ site/src/pages/Whey/index.js
@@ -40,19 +40,51 @@ return(
 
         </div>
 
-            <div className='produtos'>
+<div className='pr'>
 
-                <div className='1°'>
-                    
-                </div>
+        <div className='l1'>
+
+            <div className='produto1'>
+               <img  src='/assets/images/1whey.png'  />
+                <p>(TOP) WHEY PROTEIN CONCENTRADO (1KG) - BCCM SUPPLEMENTS</p>
+                <h1>R$ 110,00</h1>
+            </div>
+
+            <div className='produto2'>
+                <img src='/assets/images/2whey.png' />
+                <p>MEDIUM WHEY PROTEIN (1KG) - BCCM SUPPLEMENTS</p>
+                <h1>R$ 110,00</h1>
 
             </div>
+            
+        </div>
+
+        <div className='l2'>
+
+            <div className='produto3'>
+                <img src='/assets/images/3whey.png'/>
+                <p>BASIC WHEY PROTEIN (1KG) - BCCM SUPPLEMENTS</p>
+                <h1>R$ 110,00</h1>
+            </div>
+
+            <div className='produto4'>
+                <img src='/assets/images/4whey.png'/>
+                <p>(TOP) WHEY PROTEIN ISOLADO (1KG) - BCCM SUPPLEMENTS</p>
+                <h1>R$ 110,00</h1>
+            </div>
+
+        </div>
+
+            <div className='l3'>
+                <img src='/assets/images/5whey.png'/>
+                <p>3W WHEY PROTEIN (1KG) - BCCM SUPPLEMENTS</p>
+                <h1>R$ 110,00</h1>
+
+            </div>
+  </div>
+       
+  
 </div>
-        
-
-
-
-
 
     </div>
 )
