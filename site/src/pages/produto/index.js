067cd36ab377bce41425c85eb6687397dd092f19
ferraz676 <mat_ferraz12@hotmail.com>
2023-10-23 15:27:00 -0300

    ok

--- site/src/pages/produto/index.js
+++ site/src/pages/produto/index.js
@@ -8,6 +8,8 @@ export default function produto(){
 <div className='pri'>
 <Cabecalho/>
 
+
+<div className='lado'>
 <div className='conteu'>
 
     <div className='zoon'>
@@ -30,33 +32,66 @@ export default function produto(){
     </div>
 
 
-</div>
+    <div>
 
-<div>
+<div className='gor'>
 
-    <div className='gor'>
-
-    <img src='/assets/images/gordo.png' alt=''/>
-    <h1>Menos gordura corporal, graças a aceleração metabólica;</h1>
-
-    </div>
-
-    <div className='for'>
-    <img src='/assets/images/forte.png' alt=''/>
-    <h1>Afasta o catabolismo, o que é providencial para muitos músculos;</h1>
-    </div>
-
-    <div className='ps'>
-    <img src='/assets/images/peso.png' alt=''/>
-    <h1>Eleva o hormônio péptico IGF-1 importante para recuperação pós treinos;</h1>
-    </div>
+<img src='/assets/images/gordo.png' alt=''/>
+<h1>Menos gordura corporal, graças a aceleração metabólica;</h1>
 
 </div>
 
+<div className='for'>
+<img src='/assets/images/forte.png' alt=''/>
+<h1>Afasta o catabolismo, o que é providencial para muitos músculos;</h1>
+</div>
 
+<div className='ps'>
+<img src='/assets/images/peso.png' alt=''/>
+<h1>Eleva o hormônio péptico IGF-1 importante para recuperação pós treinos;</h1>
+</div>
+
+</div>
 
 
 </div>
+
+
+
+
+<div className='segundapa'>
+        <div className='plvrs'>
+            <h1>Durateston 200mg/10ml</h1>
+        </div>
+
+        <div className='plvrs2'>
+            <h1>QUALIDADE INTERNACIONAL E PREÇO DE FÁBRICA</h1>
+        </div>
+
+        <div className='plvrs3'>
+            <h1>R$259,99</h1>
+        </div>
+
+        <div className='plvrs4'>
+            <h1>NO BOLETO OU PIX ou R$259,99 no cartão de crédito em até 1x de R$259,99 sem juros</h1>
+        </div>
+
+
+         <div className='cpm'>
+            <button>COMPRAR</button>
+           
+            <img src='/assets/images/carrinho.png' alt=''/>
+       
+
+         </div>
+
+        
+
+</div>
+</div>
+
+</div>
+
 
 
     )
