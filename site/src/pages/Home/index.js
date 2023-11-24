3771a8149fdc3a9f126c7dc7ff8c69aba8dc50d7
ferraz676 <mat_ferraz12@.com>
2023-11-24 00:05:20 -0300

    ok

--- site/src/pages/Home/index.js
+++ site/src/pages/Home/index.js
@@ -1,7 +1,7 @@
 import "./index.scss";
 
 import { Swiper, SwiperSlide} from 'swiper/react';
-import { Navigation, Autoplay } from 'swiper';
+import { Navigation, Autoplay } from 'swiper/modules';
 
 import 'swiper/css';
 import 'swiper/css/navigation';
